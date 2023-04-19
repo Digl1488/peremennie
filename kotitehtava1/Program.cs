5bef53af53d94dd1124146bfb2a5680eda3bd5cf
Дмитрий Николаевич <108075686+Digl1488@users.noreply.github.com>
2023-04-19 21:16:39 +0300

    Добавьте файлы проекта.

---
+++ kotitehtava1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace peremennie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /*
+            byte page = 38;
+            sbyte celsius = -28;
+            short pixel = -15666;
+            ushort hill = 64321;
+            int luck = -234634664;
+            uint magic = 3334445;
+           
+            float temper = 36.6f
+            double pi = 3.1415926535
+            
+            string text = "вы приняты"
+            
+            bool eats = true
+            
+            */
+        }
+    }
+}
